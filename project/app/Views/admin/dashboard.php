cad34e76ab0970adc1113ed6beefd030231b8677
shilpadhiman <shilpa.vertosys@gmail.com>
2021-09-06 14:29:22 +0530

    Update dashboard.php

--- project/app/Views/admin/dashboard.php
+++ project/app/Views/admin/dashboard.php
@@ -1,9 +1,36 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title> Matrimonial Signup Page Website Template | SmartEye Apps</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Marital Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="<?php echo base_url();?>/assets/css/bootstrap-3.1.1.min.css" rel='stylesheet' type='text/css' />
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+
+<!-- Custom Theme files -->
+<link href="<?php echo base_url();?>/assets/css/style.css" rel='stylesheet' type='text/css' />
+<link href='http://fonts.googleapis.com/css?family=Oswald:300,400,700' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700' rel='stylesheet' type='text/css'>
+
+<link href="<?php echo base_url();?>/assets/css/font-awesome.css" rel="stylesheet"> 
+</head>
+
+<style>
+h3{
+	color: black;
+}	
+</style>
+
 <div class="container" style="margin-top:20px;">
 <div class="row">
 <div class="panel panel-primary">
-<div class="panel-heading">Dashboard</div>
-<h1>Hello, <?= session()->get('name') ?></h1>
-<h3><a href="<?= site_url('logout') ?>">Logout</a></h3>
+<div class="panel-heading"> <?= session()->get('name') ?><h3><a href="<?= site_url('logout') ?>">Logout</a></h3></div>
+<div class="panel-body">	
+<h1>Hello,</h1>
+
 </div>
 </div>
 </div>
