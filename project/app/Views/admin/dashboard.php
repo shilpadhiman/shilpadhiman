8e1621e3881a2a53842f351b8d62be2e60ffd20d
shilpadhiman <shilpa.vertosys@gmail.com>
2021-09-06 13:46:09 +0530

    Matrimony

--- project/app/Views/admin/dashboard.php
+++ project/app/Views/admin/dashboard.php
@@ -2,7 +2,6 @@
 <div class="row">
 <div class="panel panel-primary">
 <div class="panel-heading">Dashboard</div>
-<div class="panel-body">
 <h1>Hello, <?= session()->get('name') ?></h1>
 <h3><a href="<?= site_url('logout') ?>">Logout</a></h3>
 </div>
